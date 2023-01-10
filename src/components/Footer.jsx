10e93af634e2e91d430c944f1c8f161776c24ebf
ssalinascalibar <ssalinascalibar@gmail.com>
2023-01-10 18:07:14 -0300

    mod footer data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,11 +16,11 @@ export default function Footer() {
         <Logo />
         <div className="whatsapp">
           <i class="fa-brands fa-whatsapp"></i>
-          <a href="https://wa.me/56973445276?text=Quisiera%20%20saber%20mas%20de%20tu%20aplicación" target="_blank">+569 7344 5276</a>
+          <a href="https://wa.me/56973445276?text=Quisiera%20%20saber%20mas%20de%20tu%20aplicación" target="_blank">Whatsapp</a>
         </div>
         <div className="mail">
           <i class="fa-regular fa-envelope"></i>
-          <a href="mailto:email@example.com">mario@gmail.com</a>
+          <a href="mailto:mariofelipejimenez941@gmail.com">Email</a>
         </div>
       </div>
       <div className="copyright">
