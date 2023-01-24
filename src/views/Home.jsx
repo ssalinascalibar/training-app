4cde1e2999936746a33597c31dc6343cc8e8eb29
ssalinascalibar <ssalinascalibar@gmail.com>
2023-01-24 12:51:41 -0300

    add banner img and mod art-1-img

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,6 +8,9 @@ import Header from "../components/Header";
 import target1 from "../assets/img/target-1.jpg";
 import target2 from "../assets/img/target-2.jpg";
 import descAppImg from "../assets/img/img-desc-app.png";
+import banner1 from "../assets/img/banner-1.jpeg";
+import banner2 from "../assets/img/banner-2.jpeg";
+import banner3 from "../assets/img/banner-3.jpeg";
 
 //css
 import home from "../assets/css/home.css";
@@ -53,6 +56,47 @@ export default function Home() {
         </div>
 
         <div className="container">
+          <section className="banner">
+            <div className="row g-3">
+              <div className="col-sm-12 col-md-6">
+                <img src={banner1} alt="" />
+              </div>
+              <div className="col-sm-12 col-md-6">
+                <img src={banner2} alt="" />
+              </div>
+              <div className="col-sm-12 col-md-6">
+                <img src={banner3} alt="" />
+              </div>
+              <div className="col-sm-12 col-md-6">
+                <img src={banner2} alt="" />
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div className="container">
+          <section className="targets">
+            <h4>Entrenamiento personalizado</h4>
+            <div className="row mb-2">
+              <div className="col">
+                <img src={target1} alt="objetivos de la app mj" />
+              </div>
+              <div className="col">
+                <img src={target2} alt="objetivos de la app mj" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <img src={target2} alt="objetivos de la app mj" />
+              </div>
+              <div className="col">
+                <img src={target1} alt="objetivos de la app mj" />
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div className="container">
           <section className="work-on-you">
             <h1>Trabaja tu</h1>
             <div className="benefits">
@@ -82,28 +126,6 @@ export default function Home() {
             </p>
           </div>
         </article>
-
-        <div className="container">
-          <section className="targets">
-            <h4>Entrenamiento personalizado</h4>
-            <div className="row mb-2">
-              <div className="col">
-                <img src={target1} alt="objetivos de la app mj" />
-              </div>
-              <div className="col">
-                <img src={target2} alt="objetivos de la app mj" />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col">
-                <img src={target2} alt="objetivos de la app mj" />
-              </div>
-              <div className="col">
-                <img src={target1} alt="objetivos de la app mj" />
-              </div>
-            </div>
-          </section>
-        </div>
 
         {/* Article 2 */}
         <article className="art">
